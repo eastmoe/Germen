3ca44860d947cbc320181ed583f4a16ab8354708
Staroom <31646341+eastmoe@users.noreply.github.com>
2023-03-06 10:22:23 +0800

    优化细节

--- Click.py
+++ Click.py
@@ -2,14 +2,17 @@
 import numpy as np
 import pyautogui
 
+def ClickToNextPage():
+    xyclickplot = np.load('.//data/ClickPlot.npy', allow_pickle=True).item()
+    # 从文件加载坐标
 
-xyclickplot = np.load('.//data/ClickPlot.npy',allow_pickle=True).item()
-#从文件加载坐标
+    print(xyclickplot)
 
-print(xyclickplot)
+    x = xyclickplot.get("x")
+    y = xyclickplot.get("y")
+    # 将坐标赋予变量
 
-x=xyclickplot.get("x")
-y=xyclickplot.get("y")
-#将坐标赋予变量
+    pyautogui.click(x, y)
 
-pyautogui.click(x,y)
+    return 0
+
