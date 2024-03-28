e19475df0f1cc1292f36973e4ba2d1afcd638856
Staroom <31646341+eastmoe@users.noreply.github.com>
2024-03-28 15:15:15 +0800

    添加基于AI的格式修正（测试）

---
+++ FormatwithLLM.py
@@ -0,0 +1,32 @@
+import openai
+import json
+
+# 加载配置文件里的URL以及key和模型的配置信息
+with open("config.json", encoding='gb18030') as config:
+    ConfigInfo = json.load(config)
+    OpenAIURL=ConfigInfo["OpenAIURL"]
+    LLMModel=ConfigInfo["LLMmodelName"]
+    OpenAIAPIKEY=ConfigInfo["OpenAIAPIKEY"]
+
+# 设置OpenAI API key
+openai.api_key = OpenAIAPIKEY
+
+# 指定API端点
+openai.api_endpoint = OpenAIURL
+
+# 函数，用于修饰文本格式
+def FormatTextWithAI(OriText):
+    # 组织语言
+    send_msg="整理以下文本格式并按照正确分段返回，不得添加、减少任何字或者出现此以外的其他内容："+OriText
+    # API请求
+    response = openai.Completion.create(
+        model=LLMModel,
+        prompt=send_msg,
+        temperature=0,  # 完全确定性
+        top_p=0.01,  # 非常低的值，减少随机性
+        frequency_penalty=0,  # 不改变高频词的使用
+        presence_penalty=0  # 不改变低频词的使用
+    )
+    # 返回生成的文本
+    return response.choices[0].text
+
